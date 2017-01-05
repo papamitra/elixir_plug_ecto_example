33e5e2d7df8094630340f6398de5e50a5a6ccde1
papamitra <papamitra@gmail.com>
2017-01-05 22:26:40 +0900

    Create table.
    mix ecto.migrate

--- priv/repo/migrations/20170105132212_create_users.exs
+++ priv/repo/migrations/20170105132212_create_users.exs
@@ -2,5 +2,10 @@ defmodule Helloplug.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      timestamps
+    end
   end
 end
